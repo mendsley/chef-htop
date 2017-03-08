4c753c28d3389deeb1043135b5f0b5a3717d7f35
Matthew Endsley <mendsley@gmail.com>
2017-03-08 23:31:30 +0000

    Bump version to 2.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "github@phlippers.net"
 license          "MIT"
 description      "Installs htop"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "2.0.0"
+version          "2.1.0"
 
 recipe "htop", "Installs htop monitoring"
 
